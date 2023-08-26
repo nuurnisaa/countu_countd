29a0199b599de3ed6ff593ea40d7d93629f02791
Nuur <103619888+nuurnisaa@users.noreply.github.com>
2023-08-26 11:49:23 +0800

    Add files via upload

---
+++ COUNTU COUNTD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int count=0;
+	int countMax=10;
+	
+
+	while (count<=countMax){
+	
+	cout<<count<<endl;
+	count++;
+}
+
+count=countMax;
+
+while(count>=0){
+	cout<<count<<endl;
+	count--;
+}
+ return 0;
+
+}
